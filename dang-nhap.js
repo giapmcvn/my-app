4f9fe5ace186aa184ca06cdca2c61ccc89e39766
giapmcvn <vconggiap@gmail.com>
2026-05-25 09:36:03 +0700

    update

---
+++ dang-nhap.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCygeMWanp1LjJUD9B_ls6Z6nKZE4Bw7og",
+    authDomain: "vcg-store.firebaseapp.com",
+    projectId: "vcg-store",
+    storageBucket: "vcg-store.firebasestorage.app",
+    messagingSenderId: "889034854707",
+    appId: "1:889034854707:web:07de19e5f9194ac6d12e46"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
