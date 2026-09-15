7bf7c7f8d6dfae4cb2425e855a25f47bbbdec20e
giapmcvn <vconggiap@gmail.com>
2026-09-15 08:10:32 +0700

    update

---
+++ intro.js
@@ -0,0 +1,11 @@
+﻿(() => {
+  const intro = document.querySelector('.site-intro');
+  if (!intro) return;
+
+  const dismiss = () => {
+    intro.classList.add('site-intro--hidden');
+    intro.addEventListener('transitionend', () => intro.remove(), { once: true });
+  };
+
+  window.setTimeout(dismiss, 2300);
+})();
